7af2fa1efb376e12d815badefb973b757d02b9a3
D-coder <chuksjoshuaa@gmail.com>
2023-01-23 21:05:45 +0100

    WIP: Fixed Minor Changes

--- layouts/default.vue
+++ layouts/default.vue
@@ -9,18 +9,18 @@
         <div v-if="isMedianSidebarOpen" className="fixed w-72 top-0 z-[9999]">
             <Sidebar @close-sidebar-modal="isMedianSidebarOpen = false"/>
         </div>    
-        <div class="bg-main-bg min-h-screen w-full" :class="isSidebarOpen ? 'ml-72': (isMedianSidebarOpen && isSidebarOpen) ? '' : 'flex-2'">
+        <div class="bg-main-bg min-h-screen w-full" :class="isSidebarOpen ? 'app-close': (isMedianSidebarOpen && isSidebarOpen) ? '' : 'flex-2'">
             <div>
                 <AppHeader />
                 <div>
-                    <div v-if="isSidebarOpen"  class="close-button mt-[-3.1em]" >
+                    <div v-if="isSidebarOpen"  class="close-button mt-[-3.1em] left-close">
                         <Icon @click="isSidebarOpen = false" name="fa6-solid:arrow-left" class="p-2 text-4xl rounded-full bg-main-bg shadow-lg cursor-pointer hover:bg-light-gray hover:text-[#fff]"></Icon>
                     </div>
                     <div v-else  class="p-2 mt-[-3.7em] mr-[3em] hide-icon" >
                         <Icon @click="isSidebarOpen = true" name="fa6-solid:arrow-right" class="p-2 text-3xl rounded-full bg-main-bg shadow-lg cursor-pointer hover:bg-light-gray hover:text-[#fff]"></Icon>
                     </div>
 
-                    <div class="show-icon p-2 mt-[-3.7em] mr-[3em]" :class="isSidebarOpen ? 'hide-show-icon':''">
+                    <div class="show-icon p-2 mt-[-3.7em] mr-[3em]" >
                         <Icon @click="isMedianSidebarOpen = true; " name="fa6-solid:arrow-right"
                             class="p-2 text-3xl rounded-full bg-main-bg shadow-lg cursor-pointer hover:bg-light-gray hover:text-[#fff]"></Icon>
                     </div>
@@ -40,6 +40,8 @@ const isSidebarOpen = useState('isSidebarOpen', () => true)
 const isMedianSidebarOpen = useState('isMedianSidebarOpen', () => false)
 const screenSize = useState('screenSize', () => 1030)
 
-
+function left() {
+    console.log("left closed")
+}
 
 </script>
