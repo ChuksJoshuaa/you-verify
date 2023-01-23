d2d45db6c5bb6a49b4a1737f39dca7acdde77b35
D-coder <chuksjoshuaa@gmail.com>
2023-01-23 21:32:23 +0100

    WIP: Working on Main Page

---
+++ components/Task.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    Tasks
+  </div>
+</template>
+
+<script>
+
+</script>
+
+<style>
+
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -9,6 +9,24 @@ const title = ref('You-Verify')
       <Meta name="description" :content="title" />
       <Style type="text/css" children="body { background-color: #fff; }" />
     </Head>
-    <h1>Home Page</h1>
+    <div class="absolute mt-[4em] border-t-2 border-gray-300 w-full">
+        <div class="flex  px-2">
+          <Task />
+          <Map />
+          <Agents />
+        </div>
+    </div>
   </div>
 </template>
+
+
+<script>
+import Task from "../components/Task.vue"
+import Agents from "../components/Agents.vue"
+import Map from "../components/Map.vue"
+
+
+export default {
+  components: {Task, Agents, Map}
+}
+</script>
