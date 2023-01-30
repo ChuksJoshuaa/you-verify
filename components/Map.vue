906604074912b478373ae64ea5dd3b4b365cdfdc
Chuck Joshua Mbanaso <chuksjoshuaa@gmail.com>
2023-01-30 08:42:17 -0800

    WIP: Working on Mobile Responsiveness

--- components/Map.vue
+++ components/Map.vue
@@ -1,14 +1,8 @@
 <template>
   
-  <div class="flex justify-between p-3">  
-      <!-- <Icon name="fa6-solid:angle-right"
-      @click="$emit('open-task-modal')"
-        class="text-5xl rounded p-4 bg-dark-bg text-gray-50 font-bold cursor-pointer mr-[-0.2rem]"></Icon>
-      <Icon name="fa6-solid:angle-left" 
-      @click="$emit('open-agent-modal')"
-        class="text-5xl rounded p-4 bg-dark-bg text-gray-50 font-bold cursor-pointer ml-[-0.3rem]"></Icon> -->
+  <div class="">  
+    <h3></h3>
   </div>
-  
 
 </template>
 
