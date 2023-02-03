bb0e42da487dbc87d1ce6fb0f6ec1a4e3074c3ee
Chuck Joshua Mbanaso <chuksjoshuaa@gmail.com>
2023-02-03 06:04:42 +0100

    WIP: Added Open Layer Map

--- components/Map.vue
+++ components/Map.vue
@@ -1,26 +1,41 @@
 <template>
-  
-  <div >  
-    <h3></h3>
+  <div ref="map-root" style="width: 100%; height: 100%">
   </div>
-
 </template>
 
-
 <script>
+import View from 'ol/View'
+import Map from 'ol/Map'
+import TileLayer from 'ol/layer/Tile'
+import OSM from 'ol/source/OSM'
+
+// importing the OpenLayers stylesheet is required for having
+// good looking buttons!
+import 'ol/ol.css'
+
 export default {
-  // name: "Map",
+  name: 'Map',
+  components: {},
+  props: {},
+  mounted() {
+    // this is where we create the OpenLayers map
+    new Map({
+      // the map will be created using the 'map-root' ref
+      target: this.$refs['map-root'],
+      layers: [
+        // adding a background tiled layer
+        new TileLayer({
+          source: new OSM() // tiles are served by OpenStreetMap
+        }),
+      ],
 
-
-      methods: { 
-      yes(){
-          console.log("yes")
-        }
-      }
-
+      // the map view will initially show the whole world
+      view: new View({
+        zoom: 0,
+        center: [0, 0],
+        constrainResolution: true
+      }),
+    })
+  },
 }
 </script>
-
-<style>
-
-</style>
